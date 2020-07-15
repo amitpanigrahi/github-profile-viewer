eeefd8200bb35fe0bf3bc74570db889c0320e520
amitx <amitbizweb@gmail.com>
2020-07-15 19:31:11 +0530

    Updates

---
+++ precache-manifest.4b07b398817049c34ca352f2b57cb978.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbc9f4f38b2d3a244f5cecae685ab21",
+    "url": "/github-profile-viewer/index.html"
+  },
+  {
+    "revision": "0ee586ec2c8582d02f42",
+    "url": "/github-profile-viewer/static/css/main.8c3c7f47.chunk.css"
+  },
+  {
+    "revision": "02774ac7cda25dcdfef6",
+    "url": "/github-profile-viewer/static/js/2.f21b3aca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-profile-viewer/static/js/2.f21b3aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee586ec2c8582d02f42",
+    "url": "/github-profile-viewer/static/js/main.5595b599.chunk.js"
+  },
+  {
+    "revision": "009b90cb84816c3aa2f6",
+    "url": "/github-profile-viewer/static/js/runtime-main.3aad54ef.js"
+  },
+  {
+    "revision": "f48b8d47474e2ce176e42c889666ffe7",
+    "url": "/github-profile-viewer/static/media/bar_chart.f48b8d47.svg"
+  },
+  {
+    "revision": "4ddbc0f7bba9f3957176a57eb4a2a114",
+    "url": "/github-profile-viewer/static/media/close_book.4ddbc0f7.svg"
+  },
+  {
+    "revision": "04c5e461e3a0a933a9ca1e6908c319b3",
+    "url": "/github-profile-viewer/static/media/envelope.04c5e461.svg"
+  },
+  {
+    "revision": "711d8cf6728e83faf125b1e931d04ebb",
+    "url": "/github-profile-viewer/static/media/industry.711d8cf6.svg"
+  },
+  {
+    "revision": "8ba8ec4fbd5d93e969bb4d5ea6ea199f",
+    "url": "/github-profile-viewer/static/media/location_pin.8ba8ec4f.svg"
+  },
+  {
+    "revision": "993fe42b487b9071d902e331ea9cdf80",
+    "url": "/github-profile-viewer/static/media/open_book.993fe42b.svg"
+  },
+  {
+    "revision": "4c13e7c1e863f7dae3b29c4021da28cb",
+    "url": "/github-profile-viewer/static/media/star.4c13e7c1.svg"
+  },
+  {
+    "revision": "64c2252a9f7ca4f85244750a52c30e82",
+    "url": "/github-profile-viewer/static/media/user_group.64c2252a.svg"
+  }
+]);
